c98f335afbf4b3674a59864ee3e4a49f545ac8a2
Hyuns008 <gustn4984@naver.com>
2024-08-23 22:16:48 +0900

    설정 저장 추가

--- Assets/Scriptes/Manager/SettingManager.cs
+++ Assets/Scriptes/Manager/SettingManager.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,6 +17,7 @@ public class SettingManager : MonoBehaviour
     private List<Slider> sliders = new List<Slider>(); // 배경음, 효과음, 민감도 설정을 위한 슬라이더
     private List<TMP_Text> valueText = new List<TMP_Text>(); // 설정 값을 표기해줄 텍스트
     private Toggle windowToggle; // 창모드를 위한 토글
+    private Button settingSaveButton; // 설정 저장 버튼
 
     private void Awake()
     {
@@ -39,7 +42,66 @@ public class SettingManager : MonoBehaviour
         }
 
         windowToggle = settingObject.transform.GetChild(0).Find("WindowMode").GetComponent<Toggle>();
+        settingSaveButton = settingObject.transform.GetChild(0).Find("Save").GetComponent<Button>();
     }
+
+    private void Start()
+    {
+        closedButton();
+        saveSettingCheck();
+        saveButton();
+    }
+
+    /// <summary>
+    /// 설정창을 닫는 버튼
+    /// </summary>
+    private void closedButton()
+    {
+        closeButton.onClick.AddListener(() => 
+        {
+            settingObject.SetActive(false);
+        });
+    }
+
+    /// <summary>
+    /// 설정 저장이 되어있는지 확인을 하는 함수
+    /// </summary>
+    private void saveSettingCheck()
+    {
+        int count = sliders.Count;
+
+        if (PlayerPrefs.GetString("saveSetting") == string.Empty)
+        {
+            for (int iNum = 0; iNum < count; iNum++)
+            {
+                sliders[iNum].value = 0.5f;
+            }
+        }
+        else
+        {
+            string getSaveSetting = PlayerPrefs.GetString("saveSetting");
+
+            for (int iNum = 0; iNum < count; iNum++)
+            {
+                sliders[iNum].value = JsonConvert.DeserializeObject<float>(getSaveSetting);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 설정 저장 버튼
+    /// </summary>
+    private void saveButton()
+    {
+        settingSaveButton.onClick.AddListener(() => 
+        {
+            int count = sliders.Count;
+
+            string setSaveSetting = JsonConvert.SerializeObject(sliders);
+            PlayerPrefs.SetString("saveSetting", setSaveSetting);
+        });
+    }
+
 
     /// <summary>
     /// 설정 캔버스 오브젝트를 다른 스크립트에 보내주기 위한 함수
